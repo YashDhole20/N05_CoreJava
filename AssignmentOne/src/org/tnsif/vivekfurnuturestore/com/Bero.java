2e607095766b08b49c4ca7063fd99dae5778203c
Yash_IT <Yash_IT@DESKTOP-QCKJ3JU>
2023-09-25 23:46:04 +0530

    commit

---
+++ AssignmentOne/src/org/tnsif/vivekfurnuturestore/com/Bero.java
@@ -0,0 +1,34 @@
+package org.tnsif.vivekfurnuturestore.com;
+
+public abstract class Bero {
+	protected String beroType;
+	protected String beroColor;
+	protected double price;
+	
+	public abstract void calculatePrice();
+	public Bero(String beroType,String beroColor) {
+		super();
+		this.beroColor=beroColor;
+		this.beroType=beroType;
+	}
+	public String getBeroType() {
+		return beroType;
+	}
+	public void setBeroType(String beroType) {
+		this.beroType = beroType;
+	}
+	public String getBeroColor() {
+		return beroColor;
+	}
+	public void setBeroColor(String beroColor) {
+		this.beroColor = beroColor;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	 
+	
+}
---
+++ AssignmentOne/src/org/tnsif/vivekfurnuturestore/com/WoodenBero.java
@@ -0,0 +1,36 @@
+package org.tnsif.vivekfurnuturestore.com;
+
+public class WoodenBero extends Bero{
+
+	private String woodenType;
+	
+	 
+	
+	public WoodenBero(String beroType, String beroColor,String woodenType) {
+		super(beroType, beroColor);
+		this.woodenType=woodenType;
+	}
+	 
+
+	public String getWoodenType() {
+		return woodenType;
+	}
+
+	public void setWoodtype(String woodenType) {
+		this.woodenType = woodenType;
+	}
+	 
+	public void calculatePrice() {
+		if( woodenType.equals("PlyWood")) {
+			 price=15000;
+		}
+		if( woodenType.equals("TeakWood")) {
+			 price=12000;
+		}
+		if(woodenType.equals("EngineeredWood")) {
+			 price=10000;
+		}
+		
+		
+	}
+}
